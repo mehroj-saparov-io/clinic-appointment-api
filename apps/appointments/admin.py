6cb95819ae57d2f4ba1ecc24dfa1973ac7a68a73
mehroj-saparov-io <saparov.dev2026@gmail.com>
2026-02-27 02:47:12 +0500

    appointmets admin panel

--- apps/appointments/admin.py
+++ apps/appointments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "doctor",
+        "patient",
+        "timeslot",
+        "status",
+        "created_at",
+    )
+    list_filter = ("status", "doctor", "patient", "timeslot__date")
+    search_fields = ("doctor__username", "patient__username")
+    readonly_fields = ("created_at",)
+    ordering = ("-created_at",)
